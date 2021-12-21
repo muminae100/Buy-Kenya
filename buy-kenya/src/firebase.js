792c9b1e8315e7966221c6c25281a34758a9024a
muminae100 <smuminaetx100@gmail.com>
2021-12-21 10:53:17 +0300

    Create firebase.js

---
+++ buy-kenya/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDXew1U4jzy1MjRNhFdFFKHrenEEGSGsjU",
+    authDomain: "buy-kenya.firebaseapp.com",
+    projectId: "buy-kenya",
+    storageBucket: "buy-kenya.appspot.com",
+    messagingSenderId: "78144482614",
+    appId: "1:78144482614:web:ff114225eae80fa7f05797",
+    measurementId: "G-RRL36RYW9R"
+  };
